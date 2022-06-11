add7a22156ada578e241dec62778b3d964f70a69
Julia Helena Reuter <julia.h.reuter@googlemail.com>
2022-06-11 00:32:56 +0000

    Add storybook controls

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,7 @@ module.exports = {
   stories: ["../**/*.story.mdx", "../**/*.story.@(js|jsx|ts|tsx)"],
   addons: [
     "storybook-dark-mode",
+    "@storybook/addon-controls",
     {
       name: "storybook-addon-turbo-build",
       options: { optimizationLevel: 2 },
--- components/fields/FreeTextField.story.tsx
+++ components/fields/FreeTextField.story.tsx
@@ -1,7 +1,9 @@
+import { ComponentMeta } from "@storybook/react"
 import { FreeTextField } from "./FreeTextField"
 
 export default {
   title: "Free text field",
-}
+  component: FreeTextField,
+} as ComponentMeta<typeof FreeTextField>
 
 export const Usage = () => <FreeTextField label="Free text field" />
