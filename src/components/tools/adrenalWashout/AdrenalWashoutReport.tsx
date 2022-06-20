30acc38887404d19458b478253f2fb9d9fcff235
Kai Schlamp <kai.schlamp@gmail.com>
2022-06-20 07:57:11 +0000

    Work on adrenal washout tool

--- src/components/tools/adrenalWashout/AdrenalWashoutReport.tsx
+++ src/components/tools/adrenalWashout/AdrenalWashoutReport.tsx
@@ -1,15 +1,75 @@
 import { useReportData } from "../../../contexts/ReportDataContext"
+import {
+  calcAbsoluteAdrenalWashout,
+  calcRelativeAdrenalWashout,
+} from "../../../utils/adrenalWashoutCalculations"
 import { Citation } from "../../common/Citation"
 import { Paragraph } from "../../structuredReport/Paragraph"
 import { Report } from "../../structuredReport/Report"
 import { Statement } from "../../structuredReport/Statement"
 
 export const AdrenalWashoutReport = () => {
-  const data = useReportData()
+  const data = useReportData("adrenalWashout") as
+    | {
+        nonEnhanced: number | null
+        portalVenous: number | null
+        delayed: number | null
+      }
+    | undefined
+
   console.log(data)
+
+  const nonEnhanced = data?.nonEnhanced
+  const portalVenous = data?.portalVenous
+  const delayed = data?.delayed
+
+  let absoluteWashout: number | undefined
+  if (nonEnhanced != null && portalVenous != null && delayed != null) {
+    absoluteWashout = calcAbsoluteAdrenalWashout(nonEnhanced, portalVenous, delayed)
+  }
+
+  let relativeWashout: number | undefined
+  if (portalVenous != null && delayed != null) {
+    relativeWashout = calcRelativeAdrenalWashout(portalVenous, delayed)
+  }
+
+  let conclusion = ""
+  if (nonEnhanced != null && nonEnhanced < 0) {
+    conclusion =
+      "An adrenal lesion with a density <0 HU on nonenhanced CT is highly specific for lipid-rich adenoma (47% sensitive, 100% specific)" //1
+  } else if (nonEnhanced != null && nonEnhanced < 10) {
+    conclusion =
+      "An adrenal lesion with a density <10 HU on nonenhanced CT is fairly specific for lipid-rich adenoma (71% sensitive, 98% specific)" //1
+  } else if (nonEnhanced != null && nonEnhanced >= 43) {
+    conclusion =
+      "An adrenal lesion with a density >43 HU on nonenhanced CT that is neither calcified not hemorrhagic is suspicious for malignancy regardless of washout characteristics. Further diagnostic like FDG-PET/CT should be considered instead of washout imaging." // 2
+  } else if (portalVenous != null && portalVenous >= 130) {
+    conclusion =
+      "An indeterminate adrenal lesion that enhances more than 130 HU is concerning for hypervascular pheochromocytoma." // 3
+  } else {
+    if (absoluteWashout !== undefined && absoluteWashout >= 60) {
+      conclusion = ">60% washout is highly suggestive of adrenal adenoma."
+    } else {
+      conclusion =
+        "<60% washout is not confirming adrenal adenoma, alternative diagnoses should be considered."
+    }
+    if (relativeWashout !== undefined && relativeWashout >= 40) {
+      conclusion = ">40% washout is highly suggestive of adrenal adenoma."
+    }
+    //  else {
+    //   conclusion +
+    //     "<40% washout is not confirming adrenal adenoma, alternative diagnoses should be considered."
+    // }
+    // else {
+    //   conclusion = "sucks"
+    // }
+  }
 
   return (
     <Report>
+      Relative washout: {relativeWashout}
+      Absolute washout: {absoluteWashout}
+      {conclusion}
       <Citation
         title="Technical and Interpretive Pitfalls in Adrenal Imaging"
         authors="Gurinder Nandra, Oliver Duxbury, Pawan Patel, Jaymin H. Patel, Nirav Patel, and Ioannis Vlahos"
--- src/contexts/ReportDataContext.ts
+++ src/contexts/ReportDataContext.ts
@@ -2,5 +2,15 @@ import { createRequiredContext } from "../utils/createRequiredContext"
 
 type ReportDataContext = Record<string, Record<string, any>>
 
-export const [useReportData, ReportDataContextProvider] =
+const [_useReportData, ReportDataContextProvider] =
   createRequiredContext<ReportDataContext>("ReportDataContext")
+
+export { ReportDataContextProvider }
+
+export const useReportData = (moduleId?: string) => {
+  const data = _useReportData()
+  if (moduleId) {
+    return data[moduleId]
+  }
+  return data
+}
