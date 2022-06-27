9082a9d779e4c571094044f04b9f32f696365303
Kai Schlamp <kai.schlamp@gmail.com>
2022-06-27 22:11:56 +0000

    Delete field extra in adrenal washout

--- src/components/tools/adrenalWashout/AdrenalWashoutStructure.tsx
+++ src/components/tools/adrenalWashout/AdrenalWashoutStructure.tsx
@@ -1,24 +1,10 @@
 import { List } from "@mantine/core"
-import ArteriesBrain from "../../../../public/images/arteriesBrain.svg"
 import { MAX_HU, MIN_HU } from "../../../constants"
 import { NumberField } from "../../fields/NumberField"
-import { FieldInfo } from "../../structuredReport/FieldInfo"
-import { SelectOverlay } from "../../structuredReport/SelectOverlay"
 
 export const AdrenalWashoutStructure = () => (
   <>
-    <NumberField
-      id="nonEnhanced"
-      label="Non-enhanced phase HU"
-      min={MIN_HU}
-      max={MAX_HU}
-      extras={
-        <>
-          <FieldInfo title="Foo">foobar</FieldInfo>
-          <SelectOverlay svgImage={<ArteriesBrain />} />
-        </>
-      }
-    />
+    <NumberField id="nonEnhanced" label="Non-enhanced phase HU" min={MIN_HU} max={MAX_HU} />
     <NumberField id="portalVenous" label="Portal venous phase HU" min={MIN_HU} max={MAX_HU} />
     <NumberField id="delayed" label="Delayed phase HU" min={MIN_HU} max={MAX_HU} />
     <List>
