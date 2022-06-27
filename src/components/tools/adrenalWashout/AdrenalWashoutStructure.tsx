4a291dc406317d433177f3870badb7a3a0b68276
Julia Helena Reuter <julia.h.reuter@googlemail.com>
2022-06-27 13:00:37 +0000

    Initialize SelectOverlay

--- src/components/tools/adrenalWashout/AdrenalWashoutStructure.tsx
+++ src/components/tools/adrenalWashout/AdrenalWashoutStructure.tsx
@@ -1,13 +1,20 @@
 import { List } from "@mantine/core"
+import ArteriesBrain from "../../../../public/images/arteriesBrain.svg"
 import { NumberField } from "../../fields/NumberField"
 import { InfoOverlay } from "../../structuredReport/InfoOverlay"
+import { SelectOverlay } from "../../structuredReport/SelectOverlay"
 
 export const AdrenalWashoutStructure = () => (
   <>
     <NumberField
       id="nonEnhanced"
       label="Non-enhanced phase HU"
-      extras={<InfoOverlay title="Foo" content="bar" />}
+      extras={
+        <>
+          <InfoOverlay title="Foo" content="bar" />
+          <SelectOverlay svgImage={<ArteriesBrain />} />
+        </>
+      }
     />
     <NumberField id="portalVenous" label="Portal venous phase HU" />
     <NumberField id="delayed" label="Delayed phase HU" />
