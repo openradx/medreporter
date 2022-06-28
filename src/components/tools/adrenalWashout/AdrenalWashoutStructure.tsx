933b72d2394d5db6ce310e49adbc0059e0d8be51
Julia Helena Reuter <julia.h.reuter@googlemail.com>
2022-06-28 04:33:28 +0000

    Clean up adrenal-washout

--- src/components/tools/adrenalWashout/AdrenalWashoutStructure.tsx
+++ src/components/tools/adrenalWashout/AdrenalWashoutStructure.tsx
@@ -1,27 +1,15 @@
 import { List } from "@mantine/core"
-import ArteriesBrain from "../../../../public/images/arteriesBrain.svg"
 import { NumberField } from "../../fields/NumberField"
-import { InfoOverlay } from "../../structuredReport/InfoOverlay"
-import { SelectOverlay } from "../../structuredReport/SelectOverlay"
 
 export const AdrenalWashoutStructure = () => (
   <>
-    <NumberField
-      id="nonEnhanced"
-      label="Non-enhanced phase HU"
-      extras={
-        <>
-          <InfoOverlay title="Foo" content="bar" />
-          <SelectOverlay svgImage={<ArteriesBrain />} />
-        </>
-      }
-    />
+    <NumberField id="nonEnhanced" label="Non-enhanced phase HU" />
     <NumberField id="portalVenous" label="Portal venous phase HU" />
     <NumberField id="delayed" label="Delayed phase HU" />
     <List>
       <List.Item>
-        The ROI should cover approximately two-third of the lesion without including the perphery to
-        reduce partial volume averaging effects of the surrounding fat.
+        The ROI should cover approximately two-third of the lesion without including the periphery
+        to reduce partial volume averaging effects of the surrounding fat.
       </List.Item>
       <List.Item>Whenever possible the absolute washout should be preferred.</List.Item>
     </List>
