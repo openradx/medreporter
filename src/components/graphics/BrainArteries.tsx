8fbc44249330f76a8504308d121da39646661236
Kai Schlamp <kai.schlamp@gmail.com>
2022-06-29 19:43:53 +0000

    Add options to BrainArteries component

--- src/components/graphics/BrainArteries.tsx
+++ src/components/graphics/BrainArteries.tsx
@@ -1,43 +1,53 @@
 import { useStructureTranslation } from "../../hooks/useStructureTranslation"
-import BrainArteriesSvg from "../../svg/brainArteries.svg"
+import BrainArteriesSvg from "../../svg/BrainArteries.svg"
 import { FieldGraphics } from "../structuredReport/FieldGraphics"
 
 export const options = [
-  { value: "basilaris", label: "basilaris" },
-  { value: "basilarisHead", label: "basilarisHead" },
-  { value: "ACA", label: "ACA" },
-  { value: "rightV4", label: "rightV4" },
-  { value: "rightPICA", label: "rightPICA" },
-  { value: "rightSCA", label: "rightSCA" },
-  { value: "rightP1", label: "rightP1" },
-  { value: "rightP2", label: "rightP2" },
-  { value: "rightACP", label: "rightACP" },
-  { value: "rightA1", label: "rightA1" },
-  { value: "rightA2", label: "rightA2" },
-  { value: "rightACMT", label: "rightACMT" },
-  { value: "rightM1Proximal", label: "rightM1Proximal" },
-  { value: "rightM1Distal", label: "rightM1Distal" },
-  { value: "rightM1Bifurcation", label: "rightM1Bifurcation" },
-  { value: "rightM2Inferior", label: "rightM2Inferior" },
-  { value: "rightM2Superior", label: "rightM2Superior" },
-  { value: "leftV4", label: "leftV4" },
-  { value: "leftPICA", label: "leftPICA" },
-  { value: "leftSCA", label: "leftSCA" },
-  { value: "leftP1", label: "leftP1" },
-  { value: "leftP2", label: "leftP2" },
-  { value: "leftACP", label: "leftACP" },
-  { value: "leftA1", label: "leftA1" },
-  { value: "leftA2", label: "leftA2" },
-  { value: "leftACMT", label: "leftACMT" },
-  { value: "leftM1Proximal", label: "leftM1Proximal" },
-  { value: "leftM1Distal", label: "leftM1Distal" },
-  { value: "leftM1Bifurcation", label: "leftM1Bifurcation" },
-  { value: "leftM2Inferior", label: "leftM2Inferior" },
-  { value: "leftM2Superior", label: "leftM2Superior" },
+  { value: "leftM2Superior", label: "BrainArteries.leftM2Superior" },
+  { value: "leftM2Inferior", label: "BrainArteries.leftM2Inferior" },
+  {
+    value: "leftM1Bifurcation",
+    label: "BrainArteries.leftM1Bifurcation",
+  },
+  { value: "leftM1Distal", label: "BrainArteries.leftM1Distal" },
+  { value: "leftM1Proximal", label: "BrainArteries.leftM1Proximal" },
+  { value: "leftACMT", label: "BrainArteries.leftACMT" },
+  { value: "leftA1", label: "BrainArteries.leftA1" },
+  { value: "leftA2", label: "BrainArteries.leftA2" },
+  { value: "leftACI", label: "BrainArteries.leftACI" },
+  { value: "leftACP", label: "BrainArteries.leftACP" },
+  { value: "leftP2", label: "BrainArteries.leftP2" },
+  { value: "leftP1", label: "BrainArteries.leftP1" },
+  { value: "leftSCA", label: "BrainArteries.leftSCA" },
+  { value: "leftAICA", label: "BrainArteries.leftAICA" },
+  { value: "leftPICA", label: "BrainArteries.leftPICA" },
+  { value: "leftV4", label: "BrainArteries.leftV4" },
+  { value: "rightM2Superior", label: "BrainArteries.rightM2Superior" },
+  { value: "rightM2Inferior", label: "BrainArteries.rightM2Inferior" },
+  {
+    value: "rightM1Bifurcation",
+    label: "BrainArteries.rightM1Bifurcation",
+  },
+  { value: "rightM1Distal", label: "BrainArteries.rightM1Distal" },
+  { value: "rightM1Proximal", label: "BrainArteries.rightM1Proximal" },
+  { value: "rightACMT", label: "BrainArteries.rightACMT" },
+  { value: "rightA1", label: "BrainArteries.rightA1" },
+  { value: "rightA2", label: "BrainArteries.rightA2" },
+  { value: "rightACI", label: "BrainArteries.rightACI" },
+  { value: "rightACP", label: "BrainArteries.rightACP" },
+  { value: "rightP2", label: "BrainArteries.rightP2" },
+  { value: "rightP1", label: "BrainArteries.rightP1" },
+  { value: "rightSCA", label: "BrainArteries.rightSCA" },
+  { value: "rightAICA", label: "BrainArteries.rightAICA" },
+  { value: "rightPICA", label: "BrainArteries.rightPICA" },
+  { value: "rightV4", label: "BrainArteries.rightV4" },
+  { value: "ACA", label: "BrainArteries.ACA" },
+  { value: "basilarisHead", label: "BrainArteries.basilarisHead" },
+  { value: "basilaris", label: "BrainArteries.basilaris" },
 ]
 
 export const BrainArteries = () => {
-  const { t } = useStructureTranslation()
+  const { t } = useStructureTranslation("graphics")
 
   return <FieldGraphics title={t("BrainArteries.title")} svg={<BrainArteriesSvg />} labels={{}} />
 }
