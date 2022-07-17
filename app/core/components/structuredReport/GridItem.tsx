d38611f795ccd4facd2f7b718e075710eef7b353
Kai Schlamp <kai.schlamp@gmail.com>
2022-07-17 23:10:00 +0000

    More grid item widths

--- app/core/components/structuredReport/GridItem.tsx
+++ app/core/components/structuredReport/GridItem.tsx
@@ -2,15 +2,19 @@ import { ColProps, Grid } from "@mantine/core"
 import { ReactNode } from "react"
 
 interface GridItemProps {
-  size?: "lg" | "md" | "sm"
+  size?: "xs" | "sm" | "md" | "lg" | "xl"
   children: ReactNode
 }
 
 export const GridItem = ({ size = "sm", children }: GridItemProps) => {
-  let spans: ColProps = { xs: 12, md: 6, xl: 4 } // sm
-  if (size === "md") {
-    spans = { xs: 12, md: 12, xl: 6 }
+  let spans: ColProps = { xs: 12, md: 2, xl: 1 } // xs
+  if (size === "sm") {
+    spans = { xs: 12, md: 4, xl: 2 }
+  } else if (size === "md") {
+    spans = { xs: 12, md: 6, xl: 4 }
   } else if (size === "lg") {
+    spans = { xs: 12, md: 12, xl: 6 }
+  } else if (size === "xl") {
     spans = { xs: 12 }
   }
 
