f74980bf01cc0cc5da9e4194e8395a82435ec8f5
Kai Schlamp <kai.schlamp@gmail.com>
2022-07-26 22:11:57 +0000

    Fix last statement

--- app/core/components/structuredReport/Paragraph.tsx
+++ app/core/components/structuredReport/Paragraph.tsx
@@ -1,6 +1,7 @@
 import { Box } from "@mantine/core"
 import { cloneElement, isValidElement, ReactNode } from "react"
 import flattenChildren from "react-keyed-flatten-children"
+import { Statement } from "./Statement"
 
 interface ParagraphProps {
   last?: boolean
@@ -10,7 +11,7 @@ interface ParagraphProps {
 export const Paragraph = ({ last, children }: ParagraphProps) => (
   <Box>
     {flattenChildren(children).map((child, index, array) => {
-      if (isValidElement(child) && child.type === Paragraph) {
+      if (isValidElement(child) && child.type === Statement) {
         return cloneElement(child, { last: index === array.length - 1 })
       }
       return child
