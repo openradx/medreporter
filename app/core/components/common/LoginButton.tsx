dd9e8131f1612925cdc0e60d2c3e20aa6e4d47c5
Kai Schlamp <kai.schlamp@gmail.com>
2022-08-08 21:21:14 +0000

    Use another login icon

--- app/core/components/common/LoginButton.tsx
+++ app/core/components/common/LoginButton.tsx
@@ -1,7 +1,7 @@
 import { Routes } from "@blitzjs/next"
 import { ActionIcon } from "@mantine/core"
 import Link from "next/link"
-import { BiLogIn as LoginIcon } from "react-icons/bi"
+import { MdOutlineLogin as LoginIcon } from "react-icons/md"
 
 export const LoginButton = () => (
   <Link href={Routes.LoginPage()} passHref>
