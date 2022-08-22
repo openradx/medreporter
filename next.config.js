e7f7414edd5a99e07e862d8d499c02cd6c0f6614
Kai Schlamp <kai.schlamp@gmail.com>
2022-08-22 21:56:57 +0000

    Put webpack config in its own object

--- next.config.js
+++ next.config.js
@@ -4,55 +4,58 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 })
 
-module.exports = withBundleAnalyzer(
-  withBlitz({
-    reactStrictMode: true,
-    i18n: {
-      localeDetection: true,
-      locales: ["de", "en-US"],
-      defaultLocale: "en-US",
-    },
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-    webpack(config) {
-      if (config.mode === "development") {
-        // Watch changes of locales to reload i18next resources on the client
-        const { FileWatchHMRPlugin } = require("file-watch-hmr/plugin")
-        const path = require("path")
-        config.plugins.push(
-          new FileWatchHMRPlugin({
-            folders: [path.resolve(__dirname, "locales")],
-          })
-        )
-      }
+/**
+ * @type {import('@blitzjs/next').BlitzConfig}
+ **/
+const config = {
+  reactStrictMode: true,
+  i18n: {
+    localeDetection: true,
+    locales: ["de", "en-US"],
+    defaultLocale: "en-US",
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  webpack(config) {
+    if (config.mode === "development") {
+      // Watch changes of locales to reload i18next resources on the client
+      const { FileWatchHMRPlugin } = require("file-watch-hmr/plugin")
+      const path = require("path")
+      config.plugins.push(
+        new FileWatchHMRPlugin({
+          folders: [path.resolve(__dirname, "locales")],
+        })
+      )
+    }
 
-      // Load SVG graphics with SVGR
-      config.module?.rules?.push({
-        test: /\.svg$/,
-        use: [
-          {
-            loader: "@svgr/webpack",
-            options: {
-              svgoConfig: {
-                plugins: [
-                  {
-                    name: "preset-default",
-                    params: {
-                      overrides: {
-                        cleanupIDs: false,
-                        prefixIds: false,
-                        convertPathData: false,
-                      },
+    // Load SVG graphics with SVGR
+    config.module?.rules?.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgoConfig: {
+              plugins: [
+                {
+                  name: "preset-default",
+                  params: {
+                    overrides: {
+                      cleanupIDs: false,
+                      prefixIds: false,
+                      convertPathData: false,
                     },
                   },
-                ],
-              },
+                },
+              ],
             },
           },
-        ],
-      })
-      return config
-    },
-  })
-)
+        },
+      ],
+    })
+    return config
+  },
+}
+
+module.exports = withBundleAnalyzer(withBlitz(config))
