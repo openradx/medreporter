ac11452fa60a7e954d4def84a44aef62b0d3c09a
Kai Schlamp <kai.schlamp@gmail.com>
2022-09-12 16:29:40 +0000

    Improve localization utils

--- app/core/utils/localizationUtils.test.ts
+++ app/core/utils/localizationUtils.test.ts
@@ -1,4 +1,16 @@
-import { getCountryCode } from "./localizationUtils"
+import dayjs from "dayjs"
+import localizedFormat from "dayjs/plugin/localizedFormat"
+import { getCountryCode, getDateLocale } from "./localizationUtils"
+
+dayjs.extend(localizedFormat)
+
+describe("getDateLocale", () => {
+  it("should work", () => {
+    const locale = getDateLocale("de")
+    const time = dayjs(new Date(2022, 0, 1, 13, 59)).locale(locale).format("LT")
+    expect(time).toBe("13:59")
+  })
+})
 
 describe("getCountryCode", () => {
   it.each([
@@ -6,6 +18,7 @@ describe("getCountryCode", () => {
     ["en", "gb"],
     ["de", "de"],
     ["de-DE", "de"],
+    ["se", "sv"],
   ])("should get the correct country code", (localeCode, countrycode) => {
     expect(getCountryCode(localeCode)).toBe(countrycode)
   })
--- app/core/utils/localizationUtils.ts
+++ app/core/utils/localizationUtils.ts
@@ -1,25 +1,59 @@
-import "dayjs/locale/de"
+import DE from "dayjs/locale/de"
+import EN_US from "dayjs/locale/en"
+import EN_GB from "dayjs/locale/en-gb"
+import ES from "dayjs/locale/es"
+import FR from "dayjs/locale/fr"
+import PT from "dayjs/locale/pt"
+import SV from "dayjs/locale/sv"
 
-const supportedDateLocales = ["de", "en"]
+function getLanguageAndRegionCode(localeCode: string): [string, string | undefined] {
+  const match = Array.from(localeCode.trim().matchAll(/^([a-zA-Z]{2})(?:-([a-zA-Z]{2}))?$/g))
 
-export function getDateLocale(locale: string): string {
-  if (!supportedDateLocales.includes(locale)) {
-    throw new Error(`Unsupported locale "${locale}".`)
+  if (!match[0]) {
+    throw new Error(`Invalid locale code '${localeCode}'.`)
   }
-  return locale
+
+  return [match[0]?.[1].toLowerCase(), match[0]?.[2]?.toLowerCase()]
 }
 
-export function getCountryCode(languageCode: string): string {
-  const match = Array.from(languageCode.trim().matchAll(/^([a-zA-Z]{2})(?:-([a-zA-Z]{2}))?$/g))
-  let countryCode: string = match[0]?.[2] ?? match[0]?.[1] ?? ""
+const codeToLocaleMap: Record<string, ILocale> = {
+  de: DE,
+  us: EN_US,
+  en: EN_GB,
+  es: ES,
+  fr: FR,
+  pt: PT,
+  sv: SV,
+}
 
-  if (!countryCode) {
-    throw new Error(`Invalid locale code '${languageCode}'.`)
+export function getDateLocale(localeCode: string): ILocale {
+  const [languageCode, regionCode] = getLanguageAndRegionCode(localeCode)
+
+  if (regionCode && regionCode in codeToLocaleMap) {
+    return codeToLocaleMap[regionCode]
   }
 
-  if (countryCode === "en") {
-    countryCode = "gb"
+  if (languageCode in codeToLocaleMap) {
+    return codeToLocaleMap[languageCode]
   }
 
-  return countryCode.toLowerCase()
+  throw new Error(`Unsupported locale code '${localeCode}' for date formatting.`)
+}
+
+// https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+// https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes
+const languagesToCountriesMap: Record<string, string> = {
+  en: "gb",
+  se: "sv",
+}
+
+export function getCountryCode(localeCode: string): string {
+  const [languageCode, regionCode] = getLanguageAndRegionCode(localeCode)
+  let code = regionCode ?? languageCode
+
+  if (code in languagesToCountriesMap) {
+    code = languagesToCountriesMap[code]
+  }
+
+  return code.toLowerCase()
 }
