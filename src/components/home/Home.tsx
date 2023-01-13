71f26b84046626b78ecd26746a91ca0d0078c597
Julia Helena Reuter <julia.h.reuter@googlemail.com>
2023-01-13 11:14:19 +0000

    [wip] homepage

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,5 +1,9 @@
-import { Title, Text } from "@mantine/core"
+import { Title, Text, Card, Grid, Button } from "@mantine/core"
+import Link from "next/link"
+import { useSiteTranslation } from "~/hooks/useSiteTranslation"
+import { useUser } from "~/hooks/useUser"
 import { trpc } from "~/utils/trpc"
+import { LoginForm } from "../auth/LoginForm"
 
 export const Home = () => {
   const healthcheck = trpc.healthcheck.useQuery()
@@ -11,15 +15,63 @@ export const Home = () => {
     result = healthcheck.data
   }
 
+  const { t } = useSiteTranslation()
+  const user = useUser()
+  const loggedIn = !!user
+
   return (
-    <>
-      <Title align="center" mt={100}>
-        Welcome to{" "}
-        <Text inherit variant="gradient" component="span">
-          MedReporter
-        </Text>
-        <Text>{result}</Text>
-      </Title>
-    </>
+    <Grid>
+      <Grid.Col span={12}>
+        <Title align="center" mt={100}>
+          Welcome to{" "}
+          <Text inherit variant="gradient" component="span">
+            MedReporter
+          </Text>
+          <Text>{result}</Text>
+        </Title>
+      </Grid.Col>
+      {!loggedIn && (
+        <Grid.Col span={12}>
+          <LoginForm />
+        </Grid.Col>
+      )}
+      <Grid.Col sm={12} md={4}>
+        <Card shadow="sm" p="lg" radius="md" withBorder style={{ minHeight: 80 }}>
+          <Text weight={500}>{t("HomePage.tools.title")}</Text>{" "}
+          <Text size="sm" color="dimmed">
+            {t("HomePage.tools.description")}
+          </Text>
+          <Link href="/tools">
+            <Button variant="light" color="blue" fullWidth mt="md" radius="md">
+              {t("HomePage.tools.button")}
+            </Button>
+          </Link>
+        </Card>
+      </Grid.Col>
+      <Grid.Col sm={12} md={4}>
+        <Card shadow="sm" p="lg" radius="md" withBorder style={{ minHeight: 80 }}>
+          <Text weight={500}>{t("HomePage.modules.title")}</Text>{" "}
+          <Text size="sm" color="dimmed">
+            {t("HomePage.modules.description")}
+          </Text>
+          <Link href="/modules">
+            <Button variant="light" color="blue" fullWidth mt="md" radius="md">
+              {t("HomePage.modules.button")}
+            </Button>
+          </Link>
+        </Card>
+      </Grid.Col>
+      <Grid.Col sm={12} md={4}>
+        <Card shadow="sm" p="lg" radius="md" withBorder style={{ minHeight: 80 }}>
+          <Text weight={500}>{t("HomePage.editor.title")}</Text>{" "}
+          <Text size="sm" color="dimmed">
+            {t("HomePage.editor.description")}
+          </Text>
+          <Button variant="light" color="blue" fullWidth mt="md" radius="md">
+            {t("HomePage.editor.button")}
+          </Button>
+        </Card>
+      </Grid.Col>
+    </Grid>
   )
 }
