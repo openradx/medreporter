80494d224992276e9a1bd368c968ec9cfeb5ae84
Kai Schlamp <kai.schlamp@gmail.com>
2023-01-15 12:37:52 +0000

    Improve InstituteSwitcher

--- src/components/home/InstituteSwitcher.tsx
+++ src/components/home/InstituteSwitcher.tsx
@@ -1,4 +1,5 @@
-import { Loader, Select } from "@mantine/core"
+import { Loader, Select, SelectItem } from "@mantine/core"
+import { openModal } from "@mantine/modals"
 import { useAuthenticatedUser } from "~/hooks/useAuthenticatedUser"
 import { useSiteTranslation } from "~/hooks/useSiteTranslation"
 import { reloadSession } from "~/utils/session"
@@ -11,8 +12,7 @@ export const InstituteSwitcher = () => {
   const ownMemberships = trpc.common.getOwnMemberships.useQuery()
   const updateCurrentInstitute = trpc.common.updateCurrentInstitute.useMutation()
 
-  // TODO: Option to clear institute
-  const options =
+  const options: SelectItem[] =
     ownMemberships.data?.map((membership) => {
       const instituteId = membership.institute.id
       const instituteName = membership.institute.name
@@ -20,16 +20,38 @@ export const InstituteSwitcher = () => {
       return { value: instituteId, label: instituteName }
     }) ?? []
 
+  if (options.length) {
+    options.unshift({ value: "", label: t("InstituteSwitcher.noInstituteOption") })
+  }
+
   return (
     <Select
       label={t("InstituteSwitcher.label")}
+      placeholder={!options.length ? t("InstituteSwitcher.noInstituteAvailable") : undefined}
       data={options}
-      value={currentInstituteId}
+      value={currentInstituteId === null ? "" : currentInstituteId}
       disabled={!ownMemberships.isSuccess}
-      icon={ownMemberships.isLoading ? <Loader size="sm" /> : undefined}
-      onChange={async (newInstituteId) => {
-        await updateCurrentInstitute.mutateAsync({ instituteId: newInstituteId })
-        reloadSession()
+      icon={
+        ownMemberships.isLoading || updateCurrentInstitute.isLoading ? (
+          <Loader size="sm" />
+        ) : undefined
+      }
+      onChange={async (value) => {
+        const newInstituteId = value === "" ? null : value
+        updateCurrentInstitute.mutate(
+          { instituteId: newInstituteId },
+          {
+            onSuccess: () => {
+              reloadSession()
+            },
+            onError: (error) => {
+              openModal({
+                title: t("general.errorTitle"),
+                children: error.message,
+              })
+            },
+          }
+        )
       }}
     />
   )
