a2d376582ec54ba473beb2b27d0466cbd0a2335e
Kai Schlamp <kai.schlamp@gmail.com>
2023-02-16 15:58:58 +0000

    figure utils

---
+++ src/utils/figureUtils.ts
@@ -0,0 +1,70 @@
+export interface Metadata {
+  lngs: string[]
+  title: { [lng: string]: string }
+  description: { [lng: string]: string }
+  options: { [id: string]: { [lng: string]: string } }
+}
+
+function extractText(el: Element, lng: string, ns: string = "med"): string {
+  const transName = ns ? `${ns}:Trans` : "Trans"
+
+  let text = ""
+  for (const node of el.childNodes) {
+    if (node.nodeType === Node.TEXT_NODE) {
+      text += node.textContent
+    }
+    if (node.nodeName === transName) {
+      const transEl = node as Element
+      const transLng = transEl.getAttribute("lng")
+      if (transLng === lng) {
+        text += transEl.textContent
+      }
+    }
+  }
+  return text
+}
+
+export function extractMetadata(doc: Document, ns: string = "med"): Metadata {
+  let metadataEl = doc.querySelector("metadata")
+  if (!metadataEl) {
+    metadataEl = doc.createElementNS("http://www.w3.org/2000/svg", "metadata")
+  }
+
+  ns = `${ns}\\:`
+  let medReporterEl = metadataEl.querySelector(`${ns}MedReporter`)
+  if (!medReporterEl) {
+    medReporterEl = metadataEl.querySelector("MedReporter")
+    ns = ""
+  }
+
+  const lngs = medReporterEl
+    ?.getAttribute("lngs")
+    ?.split(",")
+    .map((lng) => lng.trim()) ?? ["en"]
+
+  const meta: Metadata = {
+    lngs: [],
+    title: {},
+    description: {},
+    options: {},
+  }
+
+  lngs.forEach((lng) => {
+    const titleEl = medReporterEl?.querySelector(`${ns}Title`)
+    meta.title[lng] = titleEl ? extractText(titleEl, lng) : "Untitled"
+
+    const descriptionEl = medReporterEl?.querySelector(`${ns}Description`)
+    meta.description[lng] = descriptionEl ? extractText(descriptionEl, lng) : ""
+
+    const optionEls = medReporterEl?.querySelectorAll(`${ns}Option`) ?? []
+    for (const optionEl of optionEls) {
+      const optionId = optionEl.getAttribute("id")
+      if (!optionId) continue
+
+      if (!(optionId in meta.options)) meta.options[optionId] = {}
+      meta.options[optionId][lng] = extractText(optionEl, lng)
+    }
+  })
+
+  return meta
+}
