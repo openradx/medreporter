7a897c7c8ef77d62492c7ba2fd99a967baefd2cf
Kai Schlamp <kai.schlamp@gmail.com>
2023-02-24 09:21:53 +0000

    Use stable NextAuth API

--- src/server/utils/sessionUtils.ts
+++ src/server/utils/sessionUtils.ts
@@ -1,8 +1,8 @@
 import { GetServerSidePropsContext } from "next"
-import { unstable_getServerSession } from "next-auth/next"
+import { getServerSession } from "next-auth/next"
 import { authOptions } from "~/pages/api/auth/[...nextauth]"
 
 export const getServerSideSession = async (
   req: GetServerSidePropsContext["req"],
   res: GetServerSidePropsContext["res"]
-) => await unstable_getServerSession(req, res, authOptions(req, res))
+) => await getServerSession(req, res, authOptions(req, res))
