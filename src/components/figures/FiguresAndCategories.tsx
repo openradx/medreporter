08eee6ba6da3a537c56694eb243791ea168b33a7
Kai Schlamp <kai.schlamp@gmail.com>
2023-02-26 18:59:46 +0000

    Only show button to add new resource when user logged in

--- src/components/figures/FiguresAndCategories.tsx
+++ src/components/figures/FiguresAndCategories.tsx
@@ -1,19 +1,23 @@
 import { useSiteTranslation } from "~/hooks/useSiteTranslation"
+import { useUser } from "~/hooks/useUser"
 import { NavbarActions } from "../common/NavbarActions"
 import { AddResourceButton } from "../resources/AddResourceButton"
 import { ResourcesAndCategories } from "../resources/ResourcesAndCategories"
 
 export const FiguresAndCategories = () => {
   const { t } = useSiteTranslation()
+  const user = useUser()
 
   return (
     <>
-      <NavbarActions>
-        <AddResourceButton
-          route={{ pathname: "/figures/new" }}
-          label={t("FiguresAndCategories.addFigureButtonLabel")}
-        />
-      </NavbarActions>
+      {user && (
+        <NavbarActions>
+          <AddResourceButton
+            route={{ pathname: "/figures/new" }}
+            label={t("FiguresAndCategories.addFigureButtonLabel")}
+          />
+        </NavbarActions>
+      )}
       <ResourcesAndCategories
         resourceType="FIGURE"
         filterInputLabel={t("FiguresAndCategories.filterInputLabel")}
--- src/components/modules/ModulesAndCategories.tsx
+++ src/components/modules/ModulesAndCategories.tsx
@@ -1,19 +1,23 @@
 import { useSiteTranslation } from "~/hooks/useSiteTranslation"
+import { useUser } from "~/hooks/useUser"
 import { NavbarActions } from "../common/NavbarActions"
 import { AddResourceButton } from "../resources/AddResourceButton"
 import { ResourcesAndCategories } from "../resources/ResourcesAndCategories"
 
 export const ModulesAndCategories = () => {
   const { t } = useSiteTranslation()
+  const user = useUser()
 
   return (
     <>
-      <NavbarActions>
-        <AddResourceButton
-          route={{ pathname: "/modules/new" }}
-          label={t("ModulesAndCategories.addModuleButtonLabel")}
-        />
-      </NavbarActions>
+      {user && (
+        <NavbarActions>
+          <AddResourceButton
+            route={{ pathname: "/modules/new" }}
+            label={t("ModulesAndCategories.addModuleButtonLabel")}
+          />
+        </NavbarActions>
+      )}
       <ResourcesAndCategories
         resourceType="MODULE"
         filterInputLabel={t("ModulesAndCategories.filterInputLabel")}
