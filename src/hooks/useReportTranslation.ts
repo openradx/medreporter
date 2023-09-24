acf221e326af318c48414ece07925d182c5a886d
Kai Schlamp <kai.schlamp@gmail.com>
2023-09-24 06:47:50 +0000

    Upgrade i18next

--- src/hooks/useReportTranslation.ts
+++ src/hooks/useReportTranslation.ts
@@ -2,7 +2,7 @@
 import { useTranslation, UseTranslationOptions } from "react-i18next"
 import { useI18nStructuredReport } from "~/contexts/I18nStructuredReportContext"
 
-export const useReportTranslation = (ns?: string, options?: UseTranslationOptions) => {
+export const useReportTranslation = (ns?: string, options?: UseTranslationOptions<undefined>) => {
   const {
     i18nReport: i18n,
     i18nStructure,
