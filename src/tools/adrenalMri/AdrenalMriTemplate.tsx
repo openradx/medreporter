18d4a4e19394ee4c2f43fe15157642c87c101451
Julia Helena Reuter <julia.h.reuter@googlemail.com>
2023-11-05 21:28:06 +0000

    Fix of spelling

--- src/tools/adrenalMri/AdrenalMriTemplate.tsx
+++ src/tools/adrenalMri/AdrenalMriTemplate.tsx
@@ -2,8 +2,8 @@ import { Template } from "~/components/template/Template"
 import { useMicroTranslation } from "~/hooks/useMicroTranslation"
 import { AdrenalMriReport } from "./AdrenalMriReport"
 import { AdrenalMriStructure } from "./AdrenalMriStructure"
-import toolInfo_de from "./AdrenalMriToolInfo_de.md"
-import toolInfo_en from "./AdrenalMriToolInfo_en.md"
+import toolInfo_de from "./adrenalMriToolInfo_de.md"
+import toolInfo_en from "./adrenalMriToolInfo_en.md"
 import { i18nStructure } from "./locales"
 
 export const AdrenalMriTemplate = () => {
