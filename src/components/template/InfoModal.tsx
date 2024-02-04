713840341d5b07ee8a9fbf6e52d782a2a1a61d41
Julia Helena Reuter <julia.h.reuter@googlemail.com>
2024-02-04 23:10:46 +0000

    Layout of InfoModal

--- src/components/template/InfoModal.tsx
+++ src/components/template/InfoModal.tsx
@@ -1,6 +1,7 @@
-import { Modal } from "@mantine/core"
+import { Box, Modal } from "@mantine/core"
 import Markdown from "react-markdown"
 import { useScreenSize } from "~/hooks/useScreenSize"
+import classes from "./InfoModal.module.css"
 
 interface InfoModalProps {
   opened: boolean
@@ -12,14 +13,10 @@ export const InfoModal = ({ opened, onClose, children }: InfoModalProps) => {
   const screenSize = useScreenSize()
 
   return (
-    <Modal
-      opened={opened}
-      onClose={onClose}
-      size={screenSize}
-      styles={{ header: { marginBottom: 8 } }}
-      withCloseButton={false}
-    >
-      <Markdown>{children}</Markdown>
+    <Modal opened={opened} onClose={onClose} size={screenSize} withCloseButton={false}>
+      <Box p="sm" className={classes.infoModal}>
+        <Markdown>{children}</Markdown>
+      </Box>
     </Modal>
   )
 }
--- src/tools/fleischner2017/fleischner2017RiskFactors_en.md
+++ src/tools/fleischner2017/fleischner2017RiskFactors_en.md
@@ -4,8 +4,8 @@
 - Exposure to asbestos, radium or uranium
 - Family history of lung cancer
 - Older age
-- "Sex: Female"
-- "Race: Black and native Hawaiian"
+- Sex: Female
+- Race: Black and native Hawaiian
 - Marginal spiculation
 - Multiplicity (under 5 nodules increases risk for malignancy)
 - Emphysema and pulmonary fibrosis (particularly IPF)
