2e66555a8b40ef6a4f930fddcabff4715cf973e7
Julia Helena Reuter <julia.h.reuter@googlemail.com>
2024-05-25 21:37:49 +0000

    Update codeSchema for validation of code

--- src/schemas/common.ts
+++ src/schemas/common.ts
@@ -1,7 +1,18 @@
 import { z } from "zod"
+import { eslintLinter, config } from "~/utils/linting"
 
 export const nodeSchema = z.object({
   nodeId: z.string(),
 })
 
-export const codeSchema = z.string().trim().max(10000)
+export const codeSchema = z
+  .string()
+  .trim()
+  .max(10000)
+  .refine(
+    (code) => {
+      const messages = eslintLinter.verify(code, config)
+      return !messages.some((message) => message.severity === 2)
+    },
+    { message: "Invalid code" }
+  )
