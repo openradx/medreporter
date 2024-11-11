78a4be74653303da921941fe3e18108ce4f9b67d
Julia Helena Reuter <julia.h.reuter@googlemail.com>
2024-11-11 22:09:52 +0000

    Hint storybook

---
+++ src/components/template/Hint.stories.tsx
@@ -0,0 +1,34 @@
+import { Meta, StoryObj } from "@storybook/react"
+import { Hint } from "./Hint"
+
+const meta: Meta<typeof Hint> = {
+  title: "Template / Hint",
+  component: Hint,
+}
+
+export default meta
+type Story = StoryObj<typeof Hint>
+
+export const Info: Story = {
+  args: {
+    level: "info",
+    content: "This is an informative hint",
+    hidden: false,
+  },
+}
+
+export const Warning: Story = {
+  args: {
+    level: "warning",
+    content: "This is a warning hint",
+    hidden: false,
+  },
+}
+
+export const Error: Story = {
+  args: {
+    level: "error",
+    content: "This is an error hint",
+    hidden: false,
+  },
+}
