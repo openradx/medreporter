35d752eb860229df530910c077573a992974b93f
julihereu <julia.h.reuter@gmail.com>
2024-12-13 22:41:42 +0000

    Changing backgroundImage with transition

--- src/components/common/BackgroundImage.tsx
+++ src/components/common/BackgroundImage.tsx
@@ -1,16 +1,37 @@
 import { Overlay } from "@mantine/core"
 import Image from "next/image"
-import bgImage from "../../../public/exampleImage2.png"
+import { useEffect, useState } from "react"
+import bgImage2 from "../../../public/exampleImage2.png"
+import bgImage from "../../../public/exampleImage.png"
+import classes from "./BackgroundImage.module.css"
 
-export const BackgroundImage = () => (
-  <>
-    <Overlay color="#000" opacity={0.65} zIndex={1} />
-    <Image
-      src={bgImage}
-      alt="Background"
-      fill
-      sizes="100vw"
-      style={{ objectFit: "cover", zIndex: -1, opacity: 0.5 }}
-    />
-  </>
-)
+export const BackgroundImage = () => {
+  const images = [bgImage, bgImage2]
+
+  const [currentImageIndex, setCurrentImageIndex] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length)
+    }, 5000)
+    return () => clearInterval(interval)
+  }, [images.length])
+  return (
+    <>
+      <Overlay color="#000" opacity={0.65} zIndex={1} />
+      {images.map((image, index) => (
+        <Image
+          key={index}
+          src={image}
+          alt="Background"
+          fill
+          sizes="100vw"
+          style={{ objectFit: "cover", zIndex: -1 }}
+          className={`${classes.background} ${
+            index === currentImageIndex ? classes.visible : classes.hidden
+          }`}
+        />
+      ))}
+    </>
+  )
+}
