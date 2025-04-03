d28de4a0ba18880a130042566b1be3923b2e0f4f
Kai Schlamp <kai.schlamp@gmail.com>
2025-04-03 21:42:06 +0000

    Use eslint browserify 9

--- src/utils/linting.ts
+++ src/utils/linting.ts
@@ -1,17 +1,24 @@
-import * as eslint from "eslint-linter-browserify"
+import js from "@eslint/js"
+import { Linter } from "eslint-linter-browserify"
 
-export const eslintLinter = new eslint.Linter()
+export const eslintLinter = new Linter()
 
-export const config: eslint.Linter.Config = {
+const errorRecommendedRules = Object.fromEntries(
+  Object.entries(js.configs.recommended.rules).map(([rule, value]) => {
+    if (Array.isArray(value)) {
+      return [rule, [2, ...value.slice(1)]]
+    }
+    return [rule, 2]
+  })
+)
+
+export const config: Linter.Config = {
   languageOptions: {
     parserOptions: { ecmaVersion: 2020, sourceType: "script" },
     globals: { fields: "readonly" },
   },
   rules: {
     semi: ["error", "never"],
+    ...errorRecommendedRules,
   },
 }
-
-eslintLinter.getRules().forEach((desc: any, name: string) => {
-  if (desc.meta.docs.recommended) config.rules![name] = 2
-})
