f7ad51bdf6d95e90f33d649689e3c81e579c7a2f
julihereu <julia.h.reuter@gmail.com>
2025-07-22 20:50:55 +0000

    Edit template button in list templates

--- src/components/templates/TemplateItem.tsx
+++ src/components/templates/TemplateItem.tsx
@@ -1,8 +1,10 @@
 import { useLingui as useLinguiLazy } from "@lingui/react"
 import { useLingui as useLinguiMacro } from "@lingui/react/macro"
-import { Badge, Card, Flex, Group, Stack, Text } from "@mantine/core"
+import { ActionIcon, Badge, Card, Flex, Group, Stack, Text } from "@mantine/core"
+import { Pencil as EditIcon } from "lucide-react"
 import Link from "next/link"
 import { CATEGORIES } from "~/constants/lazy-translations"
+import { useUser } from "~/hooks/useUser"
 import { ItemFlagIcon } from "./ItemFlagIcon"
 import classes from "./TemplateItem.module.css"
 
@@ -27,6 +29,7 @@ export const TemplateItem = ({
 }: ToolCardProps) => {
   const { t } = useLinguiMacro()
   const { _ } = useLinguiLazy()
+  const user = useUser()?.username
 
   return (
     <Link
@@ -36,10 +39,26 @@ export const TemplateItem = ({
     >
       <Card withBorder>
         <Stack gap={4} h={130} justify="space-between">
-          <Flex justify="space-between">
-            <Text fw={500} size="xl" truncate="end">
-              {title}
-            </Text>
+          <Flex justify="space-between" align="center">
+            <Flex gap={8} align="center">
+              <Text fw={500} size="xl" truncate="end">
+                {title}
+              </Text>
+              {user === username && (
+                <Link
+                  href={{
+                    pathname: "/templates/[username]/[slug]",
+                    query: { username, slug, edit: "true" },
+                  }}
+                  passHref
+                  className={classes.legacyLink}
+                >
+                  <ActionIcon variant="subtle" color="gray" title={t`Edit template`}>
+                    <EditIcon size={20} />
+                  </ActionIcon>
+                </Link>
+              )}
+            </Flex>
             <ItemFlagIcon language={language} />
           </Flex>
           <Text size="sm" c="dimmed" lineClamp={2}>
@@ -56,13 +75,13 @@ export const TemplateItem = ({
             </Group>
             <Stack gap={2} align="flex-end">
               <Group>
-                <Text size="xs">{t`Authored by:`}:</Text>
+                <Text size="xs">{t`Authored by:`}</Text>
                 <Text size="xs" fw={500}>
                   {username}
                 </Text>
               </Group>
               <Group>
-                <Text size="xs">{t`Last modified:`}:</Text>
+                <Text size="xs">{t`Last modified:`}</Text>
                 <Text size="xs" fw={500}>
                   {updatedAt.toLocaleDateString()}
                 </Text>
